79a5fbb71d5de02918560ca9989aa9bc98f13bf9
tommygun-hyphen <eromex02@gmail.com>
2022-05-28 15:24:09 +0100

    ff

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -40,13 +40,6 @@ router.post('/register', busboy(),(req, res)=>{
         }
     });
     req.busboy.on('file', (fieldName, file, fileName)=>{
-        // file.resume();
-        // return;
-        try {
-            fs.mkdirSync(path.join(__dirname, '/public/img/profile'));
-          } catch (err) {
-            if (err.code !== 'EEXIST') throw err
-          }
         let imgPath = path.join('img', 'profile', uuidv4()+'.'+fileName.filename.split('.').pop());
         let savePath = path.join('/app/public',imgPath);
         if(fieldName == 'picture' && fileName.filename){
